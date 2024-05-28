2a791121ab39f4cc1c087246bc9d5ace5badd7d8
RosyDiVilio <rosydivilio@gmail.com>
2024-05-28 19:01:17 +0200

    add: codici sconto

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const calc = document.getElementById('calculator');
 const finalPriceContainer = document.getElementById('finalPrice-container');
 let select = document.getElementById('job');
 select = job.value;
+let codeNames = document.getElementById('code');
+codeNames = code.value;
 
 const projectHours = 10;
 const backEnd = 20.50;
@@ -12,6 +14,8 @@ const analysis = 33.60;
 const userBack = 'BackEnd Development';
 const userFront = 'FrontEnd Development';
 const userAnalysis = 'Project Analysis';
+const sale = 25;
+const saleNames = ['YHDNU32','JANJC63','PWKCN25','SJDPO96','POCIE24'];
 
 let price = 0;
 
@@ -23,9 +27,19 @@ calc.addEventListener('submit', function (event) {
       } else if (job.value === userFront) {
          price = frontEnd * projectHours;
       } else if (job.value === userAnalysis) {
-         price = analysis * projectHours
+         price = analysis * projectHours;
       }
 
    finalPriceContainer.classList.remove('d-none');
    finalPrice.innerText = `€ ${price.toFixed(2)}`;
+
+   let calcSale = (price * 25) / 100;
+   let discoutPrice = (price - calcSale); 
+
+   saleNames.forEach(function (element) {
+      if (code.value === element) {
+         finalPrice.innerText = `€ ${discoutPrice.toFixed(2)}`;
+      }
+   }) 
+
 })
